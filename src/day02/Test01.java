a8dca1ec8994d63fdf6221b85287daaa68a05851
余洋 <yuyang_keep@163.com>
2019-04-03 16:05:11 +0800

    JavaDemo首次提交

--- src/day02/Test01.java
+++ src/day02/Test01.java
@@ -1,10 +1,17 @@
 package day02;
 
+import org.junit.Test;
+
 public class Test01 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+//	编写一个Student类，包含name、gender、age、id、score属性，
+	//分别为String、String、int、int、double类型。
+//	类中声明一个say方法，返回String类型，方法返回信息中包含所有属性值。
+//	在另一个StudentTest类中的main方法中，创建Student对象，
+	//并访问say方法和所有属性，并将调用结果打印输出。
+	@Test
+	public void test01() {}
 
 }
+
+
